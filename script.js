c8e5bbd805845e30321733648c7f10da2ca5fc01
Omar Jarid <omarjarid1993@gmail.com>
2025-06-11 10:25:18 +0200

    Add updateCart() and remove() functions

--- script.js
+++ script.js
@@ -9,11 +9,70 @@ const showCatalog = () => {
             <h3>${product.name}</h3>
             <div class="product-details">
                 <p>€${product.price.toFixed(2)}</p>
-                <button class="product-button" onclick="addToCart(${product.id})">Add to Cart</button>
+                <button class="product-button" data-id="${product.id}" onclick="addToCart(${product.id})">Add to Cart</button>
             </div>
         `;
         catalogContainer.appendChild(productCard);
     });
+}
+
+const updateCart = () => {
+    const cartContainer = document.getElementById("cart-items");
+    cartContainer.innerHTML = ""; // Clear previous content
+    if (!cart.length) {
+        cartContainer.innerHTML = "<p>Your cart is empty.</p>";
+        return;
+    }
+
+    cart.forEach(item => {
+        const cartItem = document.createElement("div");
+        cartItem.className = "cart-item";
+        cartItem.innerHTML = `
+            <img class="cart-item-picture" src="${item.imageUrl}" alt="${item.name}">
+            <div class="cart-item-details">
+                <h3>${item.name}</h3>
+                <p>Quantity: ${item.quantity}</p>
+            </div>
+            <div class="cart-item-details">
+                <p>Subtotal: €${(item.price * item.quantity).toFixed(2)}</p>
+                <button class="cart-item-button" data-id="${item.id}" onclick="remove(${item.id})">Remove</button>
+            </div>
+        `;
+        cartContainer.appendChild(cartItem);
+    });
+}
+
+const updateTotal = () => {}
+
+const remove = (productId) => {
+    const itemIndex = cart.findIndex(item => item.id === productId);
+    if (itemIndex > -1) {
+        cart.splice(itemIndex, 1);
+        updateCart();
+        updateTotal();
+    } else { console.error("Item not found in cart"); }
+}
+
+const addToCart = (productId) => {
+    const product = productCatalog.find(p => p.id === productId);
+    if (product) {
+        const cartItem = cart.find(item => item.id === productId);
+        if (cartItem) { 
+            cartItem.quantity++;
+        } else { 
+            cart.push({ 
+                id: product.id,
+                name: product.name,
+                price: product.price,
+                category: product.category,
+                imageUrl: product.imageUrl, 
+                quantity: 1 
+            }); 
+        }
+
+        updateCart();
+        updateTotal();
+    } else { console.error("Product not found"); }
 }
 
 const productCatalog = [
